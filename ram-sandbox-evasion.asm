9bee52bc3c91d6892b1290f7c1e5e43b4f8da3ff
arebel-arch-lenovo <accidentalrebel@gmail.com>
2022-08-09 10:47:22 +0400

    Can now pass the memory to use for the function.

--- ram-sandbox-evasion.asm
+++ ram-sandbox-evasion.asm
@@ -1,14 +1,28 @@
 	bits 64
 	default rel
 
+segment	.bss
+	memory_size resb	64
+	
 segment .text
 	global main
 	extern ExitProcess
+	extern GetPhysicallyInstalledSystemMemory
+	extern GetLastError
 
 main:
 	push    rbp
 	mov     rbp, rsp
 	sub	rsp, 32
+
+	lea	rcx, [memory_size]
+	sub	rsp, 32
+	call	GetPhysicallyInstalledSystemMemory
+	add	rsp, 32
+
+	sub	rsp, 32
+	call	GetLastError
+	add	rsp, 32
 
 	xor     rax, rax
 	call    ExitProcess
