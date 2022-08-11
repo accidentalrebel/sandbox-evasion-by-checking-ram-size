a3a7c3d8338950c41bf7e87987bd110b046d816c
arebel-arch-lenovo <accidentalrebel@gmail.com>
2022-08-11 22:32:56 +0400

    Working GlobalMemoryStatusEx code.

--- ram-sandbox-evasion-2.asm
+++ ram-sandbox-evasion-2.asm
@@ -4,9 +4,6 @@
 segment .data
 	msg_memory_size db	"Memory size: %lld", 0xd, 0xa, 0
 	
-segment	.bss
-
-	
 segment .text
 	global main
 	extern ExitProcess
@@ -16,9 +13,29 @@ segment .text
 main:
 	push    rbp
 	mov     rbp, rsp
-	sub	rsp, 32
 
+	sub	rsp, 0x40	; Reserve space for struct on stack
 	
+	mov	rax, 0x40
+	mov	[rsp], rax
+	lea	rcx, [rsp]
 
+	sub	rsp, 32		; Reserve shadow space
+	call	GlobalMemoryStatusEx
+	add	rsp, 32		; Release shadow space
+
+	xor 	rdx, rdx
+	mov	rax, [rsp+0x8]
+	mov	rcx, 1024
+	div	rcx		
+
+	mov	rdx, rax        ; Argument 2; Result of ullTotalPhys / 1024
+	lea	rcx, [msg_memory_size]  ; Argument 1; Format string
+	sub	rsp, 32		; Reserve shadow space
+	call	printf
+	add	rsp, 32		; Release shadow space
+
+	add	rsp, 0x40	; Release space of struct from stack
+	
 	xor     rax, rax
 	call    ExitProcess
